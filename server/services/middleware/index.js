92bd6e0aac1e2828fa59ff2b0b627fc499b305dc
jaimemendozadev <jaimemendozadev@gmail.com>
2018-09-30 08:37:30 -0700

    Fix linting error

--- server/services/middleware/index.js
+++ server/services/middleware/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const publicDir = path.resolve(__dirname, '../../../public');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const publicDir = path.resolve(__dirname, "../../../public");
+const randomString = "random string";
 
 module.exports = app => {
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({extended: false}));
-	app.use(express.static(publicDir));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(express.static(publicDir));
 };
