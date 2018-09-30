01b6796981d8196caff9373ad782c624b761f207
jaimemendozadev <jaimemendozadev@gmail.com>
2018-09-30 09:08:33 -0700

    Make change to lint rules

--- dev/App/index.jsx
+++ dev/App/index.jsx
@@ -1,14 +1,10 @@
-import {Component} from 'inferno';
-
+import { Component } from "inferno";
 
 class App extends Component {
   render() {
-    const {message} = this.props
-    return (
-      <h1>{message ? message : "Hey There!"}</h1>
-    )
+    const { message } = this.props;
+    return <h1>{message ? message : "Hey There!"}</h1>;
   }
 }
-
 
 export default App;
