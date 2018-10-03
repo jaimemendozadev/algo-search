dab430279ada2f735768213348b42fc4c7bceda8
jaimemendozadev <jaimemendozadev@gmail.com>
2018-10-03 12:50:03 -0700

    Continue work on FaceList

--- dev/App/components/FacetList.jsx
+++ dev/App/components/FacetList.jsx
@@ -1,11 +1,25 @@
 import { Component, render } from "inferno";
 
 class FacetList extends Component {
+  renderCategories = faceListArray => {
+    return faceListArray.map(({ name, count }, idx) => (
+      <div className="category-tag" key={`${idx}-${name}-${count}`}>
+        {name}: {count}
+      </div>
+    ));
+  };
+
+  componentDidMount = () => {
+    const { facets } = this.props;
+  };
+
   render() {
+    const { facets } = this.props;
     console.log("this.props inside FacetList ", this.props);
     return (
       <div className="facelist-container">
         <h1>FacetList</h1>
+        {facets.length > 0 ? this.renderCategories(facets) : ""}
       </div>
     );
   }
