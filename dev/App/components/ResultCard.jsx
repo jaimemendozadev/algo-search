a3ad8b860d365f7f239a75f4ded6dd0ee94be29c
jaimemendozadev <jaimemendozadev@gmail.com>
2018-10-03 09:46:00 -0700

    Fix search highlight feature
    The bug was caused by improperly creating DOM
    nodes in Inferno. Discovered had to use React
    dangerouslySetInnerHTML to set HTML from
    API highlighted search values.

--- dev/App/components/ResultCard.jsx
+++ dev/App/components/ResultCard.jsx
@@ -1,25 +1,20 @@
 import Icon from "./assets/phone-icon.svg";
 
-const checkHighlight = result => {
-  const innerHTML = result._highlightResult.name.value;
-
-  if (result.name != innerHTML) {
-    const element = document.createElement("span");
-    element.innerHTML = innerHTML;
-    return element;
-  }
-
-  return result.name;
+const createMarkup = name => {
+  return { __html: name };
 };
 
-const ResultCard = ({ result }) => {
+const ResultCard = ({ result, name }) => {
   return (
     <div className="result-card">
       <div>{result.category}</div>
 
       <div className="img-name-container">
         <img src={Icon} />
-        <p>{result.name}</p>
+        <div
+          className="highlighted"
+          dangerouslySetInnerHTML={createMarkup(name)}
+        />
       </div>
     </div>
   );
--- dev/App/components/ResultsView.jsx
+++ dev/App/components/ResultsView.jsx
@@ -3,9 +3,10 @@ import ResultCard from "./ResultCard.jsx";
 const ResultsView = ({ hits }) => {
   return (
     <div className="results-view-container">
-      {hits.map(result => (
-        <ResultCard key={result.objectID} result={result} />
-      ))}
+      {hits.map(result => {
+        const name = result._highlightResult.name.value;
+        return <ResultCard key={result.objectID} name={name} result={result} />;
+      })}
     </div>
   );
 };
