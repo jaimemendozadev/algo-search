67bf8d3b0185c7094a8a9e874e9df0dc4d356922
jaimemendozadev <jaimemendozadev@gmail.com>
2018-10-04 10:50:40 -0700

    Continue work on dropdown

--- dev/App/components/SortBy.jsx
+++ dev/App/components/SortBy.jsx
@@ -1,10 +1,27 @@
 import { Component, render } from "inferno";
 
+const defaultState = {
+  hideDropdown: false
+};
+
 class SortBy extends Component {
+  constructor(props) {
+    super(props);
+    this.state = defaultState;
+  }
+
   render() {
+    const { hideDropdown } = this.state;
     return (
       <div className="sortby-container">
-        <h1>SortBy</h1>;
+        <div className="sortby-row sortby-header-container">
+          <h1>SortBy</h1>
+        </div>
+
+        <div className={`btn-dropdown ${hideDropdown ? "hideDropdown" : ""}`}>
+          <div className="btn">Asc. Rank</div>
+          <div className="btn">Desc. Rank</div>
+        </div>
       </div>
     );
   }
