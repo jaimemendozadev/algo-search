9570ff0898f675698b99c38f7874526283dc18b5
jaimemendozadev <jaimemendozadev@gmail.com>
2018-10-06 20:38:00 -0700

    Perform successful testing of routes

--- server/services/Router/AlgoliaRouter/index.js
+++ server/services/Router/AlgoliaRouter/index.js
@@ -2,13 +2,12 @@ const express = require("express");
 const Router = express.Router();
 
 Router.post("/1/apps", (req, res) => {
-  console.log("req is ", req);
+  console.log("req.body is ", req.body);
   res.send("hit POST /api/1/apps");
 });
 
 Router.delete("/1/apps/:id", (req, res) => {
-  console.log("req is ", req);
-  console.log("req.query.id is ", req.query.id);
+  console.log("req.params.id is ", req.params.id);
   res.send("hit DELETE /api/1/apps/:id");
 });
 
