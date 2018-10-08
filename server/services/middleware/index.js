2a277b5660c9bad32f8c41561434106739b0842e
jaimemendozadev <jaimemendozadev@gmail.com>
2018-10-07 17:36:54 -0700

    Add catchall route on backend
    Catchall sends index.html file back to
    client if non-existent route is
    requested.

--- server/services/middleware/index.js
+++ server/services/middleware/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const publicDir = path.resolve(__dirname, "../../../public");
+const indexHTML = path.resolve(__dirname, "../../../public/index.html");
 const MainRouter = require("../Router");
 
 module.exports = app => {
@@ -9,4 +10,5 @@ module.exports = app => {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(express.static(publicDir));
   app.use(MainRouter);
+  app.use("*", (req, res) => res.sendFile(indexHTML));
 };
