2a54614c2c6bb322566bfa260f0f94b154d8c561
jaimemendozadev <jaimemendozadev@gmail.com>
2018-10-07 19:51:49 -0700

    Add procfile and edit package.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 const app = require("./server");
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
